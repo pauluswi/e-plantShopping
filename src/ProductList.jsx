0a128a8c3ea197abfde473c90a9b1beea70d72ce
pswied <pswied@gmail.com>
2024-11-27 10:51:25 +0700

    Test

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -259,7 +259,7 @@ const handleAddToCart = (product) => {
     dispatch(addItem(product));
     setAddedToCart((prevState) => ({
        ...prevState,
-       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart .
      }));
   };
 
