507ab1ff0913a9e73e85e27c5edf58c919e37258
pswied <pswied@gmail.com>
2024-11-27 14:13:08 +0700

    edit base vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cosmic-cassata-4f1c02",
+  base: "/e-plantShopping",
   plugins: [react()],
 })
